5f05106eff304a0f1546573beafa613fc933b107
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-12-04 18:37:45 +0100

    Dodaj pliki projektów.

---
+++ sprawPrezenty/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace sprawPrezenty
+{
+    internal class Program
+    {
+        public static char[] samogloski = { 'a', 'e', 'y', 'u', 'i', 'o' };
+        static private bool czyPrezentPopraw(string prezent)
+        {
+            int samoCount = 0;
+            if (prezent.Contains("ab") || prezent.Contains("cd") || prezent.Contains("yz"))
+                return false;
+
+            for(int i = 0; i < prezent.Length; i++){
+                if (i + 1 < prezent.Length)
+                    if (prezent[i] == prezent[i + 1])
+                        return true;
+
+                if (samogloski.Contains(prezent[i]))
+                    samoCount++;
+            }
+
+            return samoCount >= 3 ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            List<bool> popPrezent = new List<bool>();
+            int.TryParse(Console.ReadLine(), out int n);
+
+            for(int i = 0; i < n; i++){
+                popPrezent.Add(czyPrezentPopraw(Console.ReadLine()));
+            }
+
+            for(int i = 0; i < n; i++){
+                Console.WriteLine(popPrezent[i] ? "TAK" : "NIE");
+            }
+        }
+    }
+}
